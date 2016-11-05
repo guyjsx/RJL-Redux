e342c49bd4f400229d23427fae4b15d331c964fa
Guy Stitt <guyjstitt@gmail.com>
2016-11-05 15:54:59 -0400

    adding bundled files for prod

--- aurelia/config.js
+++ aurelia/config.js
@@ -222,5 +222,280 @@ System.config({
     "npm:vm-browserify@0.0.4": {
       "indexof": "npm:indexof@0.0.1"
     }
+  },
+  bundles: {
+    "aurelia.js": [
+      "github:github/fetch@1.0.0.js",
+      "github:github/fetch@1.0.0/fetch.js",
+      "github:twbs/bootstrap@3.3.7.js",
+      "github:twbs/bootstrap@3.3.7/css/bootstrap.css!github:systemjs/plugin-text@0.0.8.js",
+      "github:twbs/bootstrap@3.3.7/js/bootstrap.js",
+      "npm:aurelia-animator-css@1.0.1.js",
+      "npm:aurelia-animator-css@1.0.1/aurelia-animator-css.js",
+      "npm:aurelia-binding@1.0.9.js",
+      "npm:aurelia-binding@1.0.9/aurelia-binding.js",
+      "npm:aurelia-bootstrapper@1.0.0.js",
+      "npm:aurelia-bootstrapper@1.0.0/aurelia-bootstrapper.js",
+      "npm:aurelia-dependency-injection@1.1.0.js",
+      "npm:aurelia-dependency-injection@1.1.0/aurelia-dependency-injection.js",
+      "npm:aurelia-event-aggregator@1.0.0.js",
+      "npm:aurelia-event-aggregator@1.0.0/aurelia-event-aggregator.js",
+      "npm:aurelia-fetch-client@1.0.1.js",
+      "npm:aurelia-fetch-client@1.0.1/aurelia-fetch-client.js",
+      "npm:aurelia-framework@1.0.6.js",
+      "npm:aurelia-framework@1.0.6/aurelia-framework.js",
+      "npm:aurelia-history-browser@1.0.0.js",
+      "npm:aurelia-history-browser@1.0.0/aurelia-history-browser.js",
+      "npm:aurelia-history@1.0.0.js",
+      "npm:aurelia-history@1.0.0/aurelia-history.js",
+      "npm:aurelia-loader-default@1.0.0.js",
+      "npm:aurelia-loader-default@1.0.0/aurelia-loader-default.js",
+      "npm:aurelia-loader@1.0.0.js",
+      "npm:aurelia-loader@1.0.0/aurelia-loader.js",
+      "npm:aurelia-logging-console@1.0.0.js",
+      "npm:aurelia-logging-console@1.0.0/aurelia-logging-console.js",
+      "npm:aurelia-logging@1.0.0.js",
+      "npm:aurelia-logging@1.0.0/aurelia-logging.js",
+      "npm:aurelia-metadata@1.0.1.js",
+      "npm:aurelia-metadata@1.0.1/aurelia-metadata.js",
+      "npm:aurelia-pal-browser@1.0.0.js",
+      "npm:aurelia-pal-browser@1.0.0/aurelia-pal-browser.js",
+      "npm:aurelia-pal@1.0.0.js",
+      "npm:aurelia-pal@1.0.0/aurelia-pal.js",
+      "npm:aurelia-path@1.1.1.js",
+      "npm:aurelia-path@1.1.1/aurelia-path.js",
+      "npm:aurelia-polyfills@1.1.1.js",
+      "npm:aurelia-polyfills@1.1.1/aurelia-polyfills.js",
+      "npm:aurelia-route-recognizer@1.1.0.js",
+      "npm:aurelia-route-recognizer@1.1.0/aurelia-route-recognizer.js",
+      "npm:aurelia-router@1.0.6.js",
+      "npm:aurelia-router@1.0.6/aurelia-router.js",
+      "npm:aurelia-task-queue@1.1.0.js",
+      "npm:aurelia-task-queue@1.1.0/aurelia-task-queue.js",
+      "npm:aurelia-templating-binding@1.0.0.js",
+      "npm:aurelia-templating-binding@1.0.0/aurelia-templating-binding.js",
+      "npm:aurelia-templating-resources@1.1.1.js",
+      "npm:aurelia-templating-resources@1.1.1/abstract-repeater.js",
+      "npm:aurelia-templating-resources@1.1.1/analyze-view-factory.js",
+      "npm:aurelia-templating-resources@1.1.1/array-repeat-strategy.js",
+      "npm:aurelia-templating-resources@1.1.1/attr-binding-behavior.js",
+      "npm:aurelia-templating-resources@1.1.1/aurelia-hide-style.js",
+      "npm:aurelia-templating-resources@1.1.1/aurelia-templating-resources.js",
+      "npm:aurelia-templating-resources@1.1.1/binding-mode-behaviors.js",
+      "npm:aurelia-templating-resources@1.1.1/binding-signaler.js",
+      "npm:aurelia-templating-resources@1.1.1/compose.js",
+      "npm:aurelia-templating-resources@1.1.1/css-resource.js",
+      "npm:aurelia-templating-resources@1.1.1/debounce-binding-behavior.js",
+      "npm:aurelia-templating-resources@1.1.1/dynamic-element.js",
+      "npm:aurelia-templating-resources@1.1.1/focus.js",
+      "npm:aurelia-templating-resources@1.1.1/hide.js",
+      "npm:aurelia-templating-resources@1.1.1/html-resource-plugin.js",
+      "npm:aurelia-templating-resources@1.1.1/html-sanitizer.js",
+      "npm:aurelia-templating-resources@1.1.1/if.js",
+      "npm:aurelia-templating-resources@1.1.1/map-repeat-strategy.js",
+      "npm:aurelia-templating-resources@1.1.1/null-repeat-strategy.js",
+      "npm:aurelia-templating-resources@1.1.1/number-repeat-strategy.js",
+      "npm:aurelia-templating-resources@1.1.1/repeat-strategy-locator.js",
+      "npm:aurelia-templating-resources@1.1.1/repeat-utilities.js",
+      "npm:aurelia-templating-resources@1.1.1/repeat.js",
+      "npm:aurelia-templating-resources@1.1.1/replaceable.js",
+      "npm:aurelia-templating-resources@1.1.1/sanitize-html.js",
+      "npm:aurelia-templating-resources@1.1.1/set-repeat-strategy.js",
+      "npm:aurelia-templating-resources@1.1.1/show.js",
+      "npm:aurelia-templating-resources@1.1.1/signal-binding-behavior.js",
+      "npm:aurelia-templating-resources@1.1.1/throttle-binding-behavior.js",
+      "npm:aurelia-templating-resources@1.1.1/update-trigger-binding-behavior.js",
+      "npm:aurelia-templating-resources@1.1.1/with.js",
+      "npm:aurelia-templating-router@1.0.0.js",
+      "npm:aurelia-templating-router@1.0.0/aurelia-templating-router.js",
+      "npm:aurelia-templating-router@1.0.0/route-href.js",
+      "npm:aurelia-templating-router@1.0.0/route-loader.js",
+      "npm:aurelia-templating-router@1.0.0/router-view.js",
+      "npm:aurelia-templating@1.1.1.js",
+      "npm:aurelia-templating@1.1.1/aurelia-templating.js",
+      "npm:jquery@2.2.4.js",
+      "npm:jquery@2.2.4/dist/jquery.js"
+    ],
+    "app-build.js": [
+      "FileListToArrayValueConverter.js",
+      "ObjectKeysValueConverter.js",
+      "ObjectValuesValueConverter.js",
+      "app.html!github:systemjs/plugin-text@0.0.8.js",
+      "app.js",
+      "cases.html!github:systemjs/plugin-text@0.0.8.js",
+      "cases.js",
+      "child-nav-bar.html!github:systemjs/plugin-text@0.0.8.js",
+      "child-nav-bar.js",
+      "create-case.html!github:systemjs/plugin-text@0.0.8.js",
+      "create-case.js",
+      "create-offender.html!github:systemjs/plugin-text@0.0.8.js",
+      "create-offender.js",
+      "create-victim.html!github:systemjs/plugin-text@0.0.8.js",
+      "create-victim.js",
+      "dist/app-build.js",
+      "dist/vendor-build.js",
+      "edit-case.html!github:systemjs/plugin-text@0.0.8.js",
+      "edit-case.js",
+      "edit-offender.html!github:systemjs/plugin-text@0.0.8.js",
+      "edit-offender.js",
+      "edit-victim.html!github:systemjs/plugin-text@0.0.8.js",
+      "edit-victim.js",
+      "home.html!github:systemjs/plugin-text@0.0.8.js",
+      "home.js",
+      "main.js",
+      "nav-bar.html!github:systemjs/plugin-text@0.0.8.js",
+      "nav-bar.js",
+      "offenders.html!github:systemjs/plugin-text@0.0.8.js",
+      "offenders.js",
+      "resources/custom-select.js",
+      "tabs.html!github:systemjs/plugin-text@0.0.8.js",
+      "tabs.js",
+      "user-edit.html!github:systemjs/plugin-text@0.0.8.js",
+      "user-edit.js",
+      "user-index.html!github:systemjs/plugin-text@0.0.8.js",
+      "user-index.js",
+      "user-register.html!github:systemjs/plugin-text@0.0.8.js",
+      "user-register.js",
+      "users.html!github:systemjs/plugin-text@0.0.8.js",
+      "users.js",
+      "victim-table.html!github:systemjs/plugin-text@0.0.8.js",
+      "victim-table.js",
+      "victims.html!github:systemjs/plugin-text@0.0.8.js",
+      "victims.js",
+      "view-case.html!github:systemjs/plugin-text@0.0.8.js",
+      "view-case.js",
+      "view-offenders.html!github:systemjs/plugin-text@0.0.8.js",
+      "view-offenders.js",
+      "view-victims.html!github:systemjs/plugin-text@0.0.8.js",
+      "view-victims.js"
+    ]
+  },
+  depCache: {
+    "app.js": [
+      "aurelia-router",
+      "aurelia-http-client"
+    ],
+    "cases.js": [
+      "aurelia-framework",
+      "aurelia-http-client"
+    ],
+    "child-nav-bar.js": [
+      "aurelia-framework"
+    ],
+    "create-case.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "aurelia-router",
+      "jquery-validation",
+      "../jspm_packages/github/twbs/bootstrap@3.3.6/js/bootstrap.min.js"
+    ],
+    "create-offender.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "aurelia-router",
+      "jquery-validation",
+      "../jspm_packages/github/twbs/bootstrap@3.3.6/js/bootstrap.min.js"
+    ],
+    "create-victim.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "aurelia-router",
+      "jquery-validation",
+      "../jspm_packages/github/twbs/bootstrap@3.3.6/js/bootstrap.min.js"
+    ],
+    "edit-case.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "aurelia-router",
+      "jquery-validation",
+      "moment",
+      "datatables"
+    ],
+    "edit-offender.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "aurelia-router",
+      "jquery-validation",
+      "moment"
+    ],
+    "edit-victim.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "aurelia-router",
+      "jquery-validation",
+      "moment"
+    ],
+    "home.js": [
+      "aurelia-framework",
+      "aurelia-http-client"
+    ],
+    "main.js": [
+      "bootstrap"
+    ],
+    "nav-bar.js": [
+      "aurelia-framework"
+    ],
+    "offenders.js": [
+      "aurelia-framework",
+      "aurelia-http-client"
+    ],
+    "resources/custom-select.js": [
+      "aurelia-framework",
+      "select2"
+    ],
+    "tabs.js": [
+      "aurelia-framework",
+      "jquery",
+      "jquery-ui"
+    ],
+    "user-edit.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "aurelia-router",
+      "jquery-validation"
+    ],
+    "user-index.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "jquery",
+      "datatables"
+    ],
+    "user-register.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "aurelia-router"
+    ],
+    "users.js": [
+      "aurelia-framework",
+      "aurelia-http-client"
+    ],
+    "victim-table.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "jquery",
+      "datatables"
+    ],
+    "victims.js": [
+      "aurelia-framework",
+      "aurelia-http-client"
+    ],
+    "view-case.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "jquery",
+      "datatables"
+    ],
+    "view-offenders.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "jquery",
+      "datatables"
+    ],
+    "view-victims.js": [
+      "aurelia-framework",
+      "aurelia-http-client",
+      "jquery",
+      "datatables"
+    ]
   }
 });
